7e481b9ea20bd5940d3abb0afc61eb04d453d682
nyuta01 <nyuta2048@gmail.com>
2025-04-28 10:10:45 -0700

    chore: add env for fluid

--- src/domain/usecase/mcp.ts
+++ src/domain/usecase/mcp.ts
@@ -1,50 +1,54 @@
 import { experimental_createMCPClient as createMCPClient } from "ai";
-import { Experimental_StdioMCPTransport as StdioMCPTransport } from "ai/mcp-stdio"
+import { Experimental_StdioMCPTransport as StdioMCPTransport } from "ai/mcp-stdio";
 
 export type SSEMCPServer = {
-  type: 'sse';
-  name: string;
-  description: string;
-  url: string;
-  headers?: Record<string, string>;
-}
+	type: "sse";
+	name: string;
+	description: string;
+	url: string;
+	headers?: Record<string, string>;
+};
 
 export type StdioMCPServer = {
-  type: 'stdio';
-  name: string;
-  description: string;
-  command: string;
-  args: string[];
-  env?: Record<string, string>;
-}
+	type: "stdio";
+	name: string;
+	description: string;
+	command: string;
+	args: string[];
+	env?: Record<string, string>;
+};
 
 export type MCPServer = SSEMCPServer | StdioMCPServer;
 
 export const getTools = async (mcp: MCPServer) => {
-  switch (mcp.type) {
-    case "stdio": {
-      const client = await createMCPClient({
-        transport: new StdioMCPTransport({
-          command: mcp.command,
-          args: mcp.args,
-          env: mcp.env,
-        }),
-      });
-      const tools = await client.tools()
-      return tools;
-    }
-    case "sse": {
-      const client = await createMCPClient({
-        transport: {
-          type: "sse",
-          url: mcp.url,
-          headers: mcp.headers,
-        },
-      });
-      const tools = await client.tools()
-      return tools;
-    }
-  }
+	switch (mcp.type) {
+		case "stdio": {
+			const env = mcp.env ?? {};
+			if (process.env.IS_VERCEL_FLUID === "development") {
+				env.npm_config_cache = "/tmp/.npm-cache";
+				env.npm_config_prefix = "/tmp/.npm-global";
+				env.npm_config_tmp = "/tmp";
+			}
+			const client = await createMCPClient({
+				transport: new StdioMCPTransport({
+					command: mcp.command,
+					args: mcp.args,
+					env,
+				}),
+			});
+			const tools = await client.tools();
+			return tools;
+		}
+		case "sse": {
+			const client = await createMCPClient({
+				transport: {
+					type: "sse",
+					url: mcp.url,
+					headers: mcp.headers,
+				},
+			});
+			const tools = await client.tools();
+			return tools;
+		}
+	}
 };
-
-
